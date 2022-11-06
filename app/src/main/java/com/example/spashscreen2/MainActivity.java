2cdc85d375a3ae8778b9f2fdda645fe08f0f9df9
Rahul675 <51952402+Rahul675@users.noreply.github.com>
2022-11-06 17:31:07 +0530

    hellosunil

--- app/src/main/java/com/example/spashscreen2/MainActivity.java
+++ app/src/main/java/com/example/spashscreen2/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this, "HIIIIIIIIIiiiiii", Toast.LENGTH_SHORT).show();
     }
 }
